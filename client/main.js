73a1f4e60aff93d9c6b05128571baf7722398485
fergmac <f.macconnell@gmail.com>
2016-10-30 21:07:29 -0700

    add Meteor.startup and  renderRoutes to main.js

--- client/main.js
+++ client/main.js
@@ -3,10 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import '/imports/startup/client';
 import '../imports/startup/accounts-config.js';
-//import { renderRoutes } from '../imports/startup/client/routes.js';
-
+import { renderRoutes } from '../imports/startup/client/routes.js';
 import App from '../imports/ui/App.jsx';
  
 Meteor.startup(() => {
-  render(renderRoutes(), document.getElementById('app'));
+  render(renderRoutes(), document.getElementById('react-root'));
 });
