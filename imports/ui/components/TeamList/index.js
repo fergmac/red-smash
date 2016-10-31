731f1e85e5a2498af5ad57a02109085115750b25
fergmac <f.macconnell@gmail.com>
2016-10-30 21:02:22 -0700

    create TeamList component

---
+++ imports/ui/components/TeamList/index.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
+import { Teams } from '../../../startup/client/api-teams.js';
+import { Meteor } from 'meteor/meteor';
+
+class TeamList extends Component {
+    render() {
+        return (
+            <div>Leader Board</div>
+        );
+
+    };
+    
+}
+TeamList.propTypes = {
+  teams: PropTypes.array.isRequired,
+}
+
+export default createContainer(() => {
+  Meteor.subscribe('teams');
+  return {
+    teams: Teams.find().fetch(),
+  };
+}, TeamList);
