40176e2893207f94b501013cd28a9dd0601e2a38
fergmac <f.macconnell@gmail.com>
2016-10-31 09:55:42 -0700

    update teams collection

--- imports/startup/client/api-teams.js
+++ imports/startup/client/api-teams.js
@@ -5,7 +5,7 @@ export const Teams = new Mongo.Collection('teams');
 
 if (Meteor.isServer) {
   // This code only runs on the server
-   // Only publish tasks that are public or belong to the current user
+ 
   Meteor.publish('teams', function teamsPublication() {
     return Teams.find({
 
@@ -16,7 +16,7 @@ if (Meteor.isServer) {
 Meteor.methods({
   'teams.insert'(text) {
     check(text, String);
-    // add update method if user id matches user id update
+    
     Teams.insert({
       text,
       createdAt: new Date(),
