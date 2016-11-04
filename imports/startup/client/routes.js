aa819bec5f6340967577fd84f44f18cdb43a082d
scottclayton <i@scottc.ca>
2016-11-03 20:18:24 -0700

    fix merge conflict in mui routing

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,21 +8,16 @@ import TeamList from '../../ui/components/TeamList';
 import ChallengeDetails from '../../ui/components/ChallengeDetails';
 import { UserViewer } from '../../ui/components/UserViewer/';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import muiTheme from '../../ui/mui-theme';
-
 export const renderRoutes = () => (
-  <MuiThemeProvider muiTheme={muiTheme}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Splash} />
-        <Route path="/leaderboard" component={TeamList} />
-        <Route path="/user" component={UserViewer} />
-        <Route path="/challenges" component={ChallengeList} />
-        <Route path="/challenges/id/:id" component={ChallengeDetails} />
-      {/*  <Route path="/*" component={NotFoundPage} /> */}
-      </Route>
-    </Router>
-  </MuiThemeProvider>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Splash} />
+      <Route path="/leaderboard" component={TeamList} />
+      <Route path="/user" component={UserViewer} />
+      <Route path="/challenges" component={ChallengeList} />
+      <Route path="/challenges/id/:id" component={ChallengeDetails} />
+    {/*  <Route path="/*" component={NotFoundPage} /> */}
+    </Route>
+  </Router>
 );
 
--- imports/ui/body.js
+++
@@ -1,9 +0,0 @@
-// import { Template } from 'meteor/templating';
-//  import { Teams } from '../api/api-teams.js';
-// import '../../client/main.html';
- 
-// Template.body.helpers({
-//    teams() {
-//     return Tasks.find({});
-//   },
-// });
