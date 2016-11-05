67b26a1d1923941c539f630daa9b6a76d096699b
fergmac <f.macconnell@gmail.com>
2016-11-05 13:38:51 -0700

    style NavBar buttons

--- imports/ui/components/NavBar/index.js
+++ imports/ui/components/NavBar/index.js
@@ -2,21 +2,23 @@ import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import { browserHistory, Link } from 'react-router';
-import styles from './styles.css';
-// const styles =  {
-//     navBar: {
-        
-//     }
-// }
+import { ButtonToolbar, Button } from 'react-bootstrap';
 
-export default class NavBar extends Component{
+const styles =  {
+    buttons: {
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'center'
+    }
+}
+export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <ul className={styles.navBar}>
-        <Link to="/leaderboard">LeaderBoard</Link>
-        <Link to="/challenges">Challenges</Link>
-        </ul>
+      <div style={styles.buttons}>
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large" active><Link to="/leaderboard">Leaderboard</Link></Button>
+          <Button bsStyle="primary" bsSize="large" active><Link to="/challenges">Challenges</Link></Button>
+        </ButtonToolbar>
       </div>
     )
   }
