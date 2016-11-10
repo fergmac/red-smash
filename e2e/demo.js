3bf75170989a857bf25404c0cc6b0faa679c745c
scottclayton <i@scottc.ca>
2016-11-10 15:28:07 -0800

    add nightwatch e2e pages testing

--- e2e/demo.js
+++ e2e/demo.js
@@ -1,12 +1,15 @@
+const viewport_widths = [240, 320, 360, 568, 603, 640, 768, 800, 960, 1024, 1280, 1400, 1600, 1920];
+const imageDir = name => __dirname + '/screenshots/sizes/' + name + '.png';
+
 const url = 'http://localhost:3000/';
 module.exports = {
-  'Loads page with title': (browser) => {
-    browser
-      .url(url + 'challenges')
-      .waitForElementVisible('body', 1000)
-      .assert.containsText('h1', 'Challenges')
-      .end();
-  },
+  // 'Loads page with title': (browser) => {
+  //   browser
+  //     .url(url + 'challenges')
+  //     .waitForElementVisible('body', 1000)
+  //     .assert.containsText('h1', 'Challenges')
+  //     .end();
+  // },
   'Loads page with face': (browser) => {
     browser
       .url(url + 'leaderboard')
@@ -14,13 +17,39 @@ module.exports = {
       .assert.containsText('h1', 'Leader Board')
       .end();
   },
-  'Check length of table': (browser) => {
-    browser
-      .url(url + 'leaderboard')
+  'Loads page with title': (browser) => {
+    // before
+    // browser
+    //   .url('http://localhost:3000')
+    //   .waitForElementVisible('body', 1000)
+    //   .assert.containsText('h1', 'Worst Pokemon')
+    //   .end();
+    // with page objects
+    let main = browser.page.main();
+    main
+      .navigate()
       .waitForElementVisible('body', 1000)
-      .elements('tag name', 'tr', (result) => {
-        browser.assert.equal(result.value.length > 5, true)
-      })
-      .end();
+      .assert.containsText('@title', 'Leader Board');
+    browser.end();
   }
+  // 'Check length of table': (browser) => {
+  //   browser
+  //     .url(url + 'leaderboard')
+  //     .waitForElementVisible('body', 1000)
+  //     .elements('tag name', 'tr', (result) => {
+  //       browser.assert.equal(result.value.length > 3, true)
+  //     })
+  //     .end();
+  // }
+  // 'Take photos': (browser) => {
+  //   browser
+  //     .url(url + 'leaderboard')
+  //     .waitForElementVisible('body', 1000);
+  //   viewport_widths.forEach(width => {
+  //     browser
+  //       .resizeWindow(width, 300)
+  //       .saveScreenshot(imageDir(`page-${width}`));
+  //   });
+  //   browser.end();
+  // }
 };
