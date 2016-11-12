937f6260ab2a873574e72d5c0537fdee588efa7b
scottclayton <i@scottc.ca>
2016-11-11 23:23:00 -0800

    refactor ChallengeDetails to be responsive

--- imports/ui/components/ChallengeDetails/index.js
+++ imports/ui/components/ChallengeDetails/index.js
@@ -7,97 +7,87 @@ import { browserHistory } from 'react-router';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 
 const styles = {
-    reflectSubmit: {
-        display: 'flex',
-        flexFlow: 'column nowrap',
-        alignItems: 'center',
-    },
-    challenge: {
-        color: '#f4e17d',
-        textDecoration: 'none',
-    },
-    details: {
-        marginTop: '10rem',
-        marginBottom: '10rem',
-        display: 'flex',
-        justifyContent: 'space-around',
-    },
-    reflect: {
-        marginTop: '5rem',
-        marginBottom: '5rem',
-        width: '800px'
-    }
+  challenge: {
+    color: '#f4e17d',
+    textDecoration: 'none',
+  }
 }
 
 class ChallengeDetails extends Component {
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log(this.refs);
-  
-        Meteor.call('updateUserChallenges', {
-            userId: Meteor.userId(),
-            challengeId: this.props.challenges[0]._id,
-            reflection: this.refs.textInput.value,
-            starsEarned: this.props.challenges[0].starCount,
-            challengeName: this.props.challenges[0].name,
-            username: Meteor.users.find({ _id: Meteor.userId() }).fetch()[0].username
-        });
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.refs);
 
-        browserHistory.push('/leaderboard');
-    }
+    Meteor.call('updateUserChallenges', {
+      userId: Meteor.userId(),
+      challengeId: this.props.challenges[0]._id,
+      reflection: this.refs.textInput.value,
+      starsEarned: this.props.challenges[0].starCount,
+      challengeName: this.props.challenges[0].name,
+      username: Meteor.users.find({ _id: Meteor.userId() }).fetch()[0].username
+    });
 
-    render() {
+    browserHistory.push('/leaderboard');
+  }
 
-        console.log(this.props)
-        return (
+  render() {
+    return (
+      <div>
+        {/* this is a check if array is empty populate with following props */}
+        {
+          this.props.challenges.length ?
             <div>
-                <div>
-                    {/* this is a check if array is empty populate with following props */}
-                    {this.props.challenges.length ?
-                        <div>
-                            <h3>{this.props.challenges[0].name}</h3>
-                            <div style={styles.details}>
-                                <div style={styles.challenge} >{this.props.challenges[0].description}</div>
-                                <div>
-                                    <span>{this.props.challenges[0].starCount}</span>
-                                    <span style={styles.challenge}>✯</span>
-                                </div>
-                            </div>
-                        </div>
-                        : ''}
-                    <div>
-                        <h2>Reflection</h2>
-                        <div>
-                            {this.props.currentUser ?
-                                <form style={styles.reflectSubmit} onSubmit={this.handleSubmit.bind(this)} >
-                                    <textarea
-                                        type="text"
-                                        ref="textInput"
-                                        placeholder="Reflect"
-                                        rows="10"
-                                        style={styles.reflect}
-                                        />
-                                    <button className="btn btn-success btn-lg">Submit</button>
-                                </form> : ''}
-                        </div>
-                    </div>
+              <h3>{this.props.challenges[0].name}</h3>
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-8">
+                    <h3 style={styles.challenge}>{this.props.challenges[0].description}</h3>
+                  </div>
+                  <div className="col-lg-4">
+                    <h3>
+                      {this.props.challenges[0].starCount}
+                      <span style={styles.challenge}>✯</span>
+                    </h3>
+                  </div>
                 </div>
+              </div>
             </div>
-        );
-    }
+            : 'This challenge is empty for some strange reason'
+        }
+        <div>
+          <h2>
+            <br />
+            Reflection
+            </h2>
+          <div>
+            {
+              this.props.currentUser ?
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                  <div className="form-group">
+                    <textarea type="text" ref="textInput" className="form-control" placeholder="Reflect" rows="10" />
+                  </div>
+                  <button type="submit" className="btn btn-default btn-lg">Submit</button>
+                </form>
+                : 'You must log in to complete a challenge'
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ChallengeDetails.propTypes = {
-    challenges: PropTypes.array.isRequired,
-    currentUser: PropTypes.object,
+  challenges: PropTypes.array.isRequired,
+  currentUser: PropTypes.object,
 }
 
 export default createContainer(({ params }) => {
-    console.log(Meteor.users);
-    Meteor.subscribe('challenges', params.id);
-    const challenges = Challenges.find({ _id: params.id }).fetch() || []; // pass params.id which also gets passed into publication so that we 
-    return {                                                              // can get the challenge details of the specific challenge id Tracker magic going on here
-        challenges: challenges, //find where this.props.params.id          // this is all then passed to the props challenges
-        currentUser: Meteor.user(),     
-    }
+  console.log(Meteor.users);
+  Meteor.subscribe('challenges', params.id);
+  const challenges = Challenges.find({ _id: params.id }).fetch() || []; // pass params.id which also gets passed into publication so that we 
+  return {                                                              // can get the challenge details of the specific challenge id Tracker magic going on here
+    challenges: challenges, //find where this.props.params.id          // this is all then passed to the props challenges
+    currentUser: Meteor.user(),
+  }
 }, ChallengeDetails);
