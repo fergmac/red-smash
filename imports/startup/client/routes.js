48ff35450c8ee21f17aff265161f45edc9d3625b
scottclayton <i@scottc.ca>
2016-11-12 17:14:22 -0800

    delete UserViewer components and route

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,14 +5,12 @@ import { App } from '../../ui/App';
 import ChallengeList from '../../ui/components/ChallengeList';
 import LeaderBoard from '../../ui/components/LeaderBoard';
 import ChallengeDetails from '../../ui/components/ChallengeDetails';
-import { UserViewer } from '../../ui/components/UserViewer/';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Splash} />
       <Route path="/leaderboard" component={LeaderBoard} />
-      <Route path="/user" component={UserViewer} />
       <Route path="/challenges" component={ChallengeList} />
       <Route path="/challenges/id/:id" component={ChallengeDetails} />
     </Route>
