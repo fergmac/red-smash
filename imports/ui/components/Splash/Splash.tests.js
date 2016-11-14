5e1b8d446a203c719e5d9e788fbe56048783d3d3
scottclayton <i@scottc.ca>
2016-11-14 00:11:29 -0800

    add PlayersTable.tests for render and call

--- imports/ui/components/Splash/Splash.tests.js
+++ imports/ui/components/Splash/Splash.tests.js
@@ -6,12 +6,11 @@ import { chai } from 'meteor/practicalmeteor:chai'
 
 if (Meteor.isClient) {
   describe('component - Splash', () => {
-    it('should render', () => {
-      const item = shallow(<Splash />);
-      chai.assert.equal(item.text(), 'REDSMASH');
-      // chai.assert.strictEqual(item.hasClass('text-center'), 'RED SMASH')
-      // chai.assert.equal(item.find('.editing').length, 0);
-      // chai.assert.equal(item.find('input[type="text"]').prop('defaultValue'), 'testing');
+    describe('render', () => {
+      it('should render', () => {
+        const item = shallow(<Splash />);
+        chai.assert.equal(item.text(), 'REDSMASH');
+      });
     });
   });
 }
